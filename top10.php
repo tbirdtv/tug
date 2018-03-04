ad07d9d6772c6929eee34e87cf3f555aed8290d1
tbirdtv <37005325+tbirdtv@users.noreply.github.com>
2018-03-04 18:20:39 -0500

    Update top10.php

--- top10.php
+++ top10.php
@@ -1,10 +1,1 @@
 <NAME>Test</NAME><URL>https://cdn.amateurcool.com/videos/6/3/7/5/a/5a954f232b773.mp4</URL><ICON>https://cdn.amateurcool.com/thumbs/6/3/7/5/a/6375a954130c232a.mp4/6375a954130c232a.mp4-6.jpg</ICON><FANART>https://pre00.deviantart.net/8327/th/pre/i/2017/008/f/9/a2_nier__automata_fanart_by_letrongdao-daunkvs.jpg</FANART><DESC>Search contents</DESC>
-<NAME>Video 2</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 2</DESC>
-<NAME>Video 3</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 1</DESC>
-<NAME>Video 4</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 2</DESC>
-<NAME>Video 5</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 1</DESC>
-<NAME>Video 6</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 2</DESC>
-<NAME>Video 7</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 1</DESC>
-<NAME>Video 8</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 2</DESC>
-<NAME>Video 9</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 1</DESC>
-<NAME>Video 10</NAME><URL>http:://xxx/.php</URL><ICON></ICON><FANART></FANART><DESC>This is category 2</DESC>
